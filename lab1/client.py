f8dd43c30cf2d0ef25515b4c608f65227bbf4874
Anastasiia Tsapko <anastsapko@gmail.com>
2023-10-05 20:21:10 +0300

    task1.3

--- lab1/client.py
+++ lab1/client.py
@@ -1,13 +1,16 @@
 import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 client_socket.connect(('localhost', 12345))
 
-message = input("Введіть текст для відправлення: ")
-client_socket.send(message.encode())
+while True:
+    message = input("Введіть текст для відправки (або 'закрити з'єднання' для виходу): ")
+    client_socket.send(message.encode())
 
-response = client_socket.recv(1024).decode()
-print(f"Від сервера отримано: {response}")
+    if message == "закрити з'єднання":
+        break
+
+    response = client_socket.recv(1024).decode()
+    print(f"Від сервера отримано: {response}")
 
 client_socket.close()
--- lab1/server.py
+++ lab1/server.py
@@ -3,7 +3,6 @@ import datetime
 import time
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 server_socket.bind(('localhost', 12345))
 
 server_socket.listen()
@@ -12,16 +11,25 @@ print("Сервер чекає на підключення...")
 client_socket, client_address = server_socket.accept()
 print(f"З'єднання з {client_address}")
 
-data = client_socket.recv(1024).decode()
-current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-print(f"Отримано: {data}\nЧас отримання: {current_time}")
+while True:
+    data = client_socket.recv(1024).decode()
 
-time.sleep(5)
+    if not data:
+        break
 
-if len(data) != client_socket.send(data.encode()):
-    print("Помилка надсилання даних!")
-else:
-    print("Дані успішно надіслані!")
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Отримано: {data}\nЧас отримання: {current_time}")
+
+    if data == "закрити з'єднання":
+        print("Команда для закриття отримана. Закриваю з'єднання.")
+        break
+
+    time.sleep(5)
+
+    if len(data) != client_socket.send(data.encode()):
+        print("Помилка надсилання даних!")
+    else:
+        print("Дані успішно надіслані!")
 
 client_socket.close()
 server_socket.close()
